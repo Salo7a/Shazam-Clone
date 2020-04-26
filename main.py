1d6d325a0d2be9b0b03f91f94125a58ac71baeb3
Salo7a <salo7a98x@gmail.com>
2020-04-27 01:16:11 +0200

    Handled Some Errors

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from Utils import *
 
 dh = DatabaseHandler()
 db = dh.GetDB()
-# dh.UpdateDB()
 
 
 # dh.UpdateDB()
 
 def FindSimilar(Path):
+    print(colored(f"Reading File {Path}",
+                  "yellow"))
     spectrogram, features = getSpectrogram(Path)
     SpecHash = HashArray(spectrogram)
     FeaturesHash = HashArray(features)
@@ -42,10 +43,13 @@ def FindSimilar(Path):
 def ArraySimilarity(Arr1, Arr2, Mode="Permissive"):
     Hash1 = np.asarray(Arr1)
     Hash2 = np.asarray(Arr2)
-    if Mode == "Permissive":
-        percentage = len(set(Hash1[:, 0]) & set(Hash2[:, 0])) / float(len(set(Hash1[:, 0]) | set(Hash2[:, 0]))) * 100
+    if len(Hash1) != 0 and len(Hash2) != 0:
+        if Mode == "Permissive":
+            percentage = len(set(Hash1[:, 0]) & set(Hash2[:, 0])) / float(len(set(Hash1[:, 0]) | set(Hash2[:, 0]))) * 100
+        else:
+            percentage = len(set(Hash1) & set(Hash2)) / float(len(set(Hash1) | set(Hash2))) * 100
     else:
-        percentage = len(set(Hash1) & set(Hash2)) / float(len(set(Hash1) | set(Hash2))) * 100
+        percentage = 0
     # number_of_equal_elements = np.sum(Arr1 == Arr2)
     # total_elements = np.multiply(*Arr1.shape)
     # percentage = number_of_equal_elements / total_elements
